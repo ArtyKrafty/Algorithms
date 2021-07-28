f46900ec928a1bdc752a427c49f2e4c2bd991c7d
Arty_Solomko <artysolomko@gmail.com>
2021-07-28 16:50:56 +0300

    new_algo_graphs

---
+++ venv/graph.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+def person_is_seller(name):
+    return[-1] == 'p'
+
+def search(name):
+
+    search_queue = deque()
+    search_queue += graph [name]
+    searched = []
+
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                print (person + ' found!')
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False
+if __name__ == '__main__':
+
+    graph = {}
+    graph['You'] = {'Ann', 'John', 'ZZ_top'}
+    graph['Ann'] = {'Anuj', 'Peggy'}
+    graph['John'] = {'Peggy'}
+    graph['ZZ_top'] = {'Tom', 'Curt'}
+    graph['Anuj'] = {}
+    graph['Peggy'] = {}
+    graph['Tom'] = {}
+    graph['Curt'] = {}
+
+
+    print(search('ZZ_top'))
+
